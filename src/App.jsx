2c820eed22ad69c61480554168d6ff79080c0471
Krishanu Mandal <krishanu178@gmail.com>
2026-09-02 23:38:58 +0530

    Fix  modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { LenisProvider } from './context/LenisContext'
+import { ContactModalProvider, useContactModal } from './context/ContactModalContext'
 import Header from './components/layout/Header/Header'
 import Home from './components/sections/Home/Home'
 import About from './components/sections/About/About'
@@ -15,25 +16,34 @@ import ScrollProgressBar from './components/common/ScrollProgressBar/ScrollProgr
 import CursorGlow from './components/common/CursorGlow/CursorGlow'
 import CustomCursor from './components/common/CustomCursor/CustomCursor'
 import TechMarquee from './components/common/TechMarquee/TechMarquee'
+import ContactModal from './components/common/ContactModal/ContactModal'
+
+function ContactModalRoot() {
+	const { isOpen, close } = useContactModal();
+	return <ContactModal isOpen={isOpen} onClose={close} />;
+}
 
 function App() {
 	return (
 		<LenisProvider>
-			<ScrollProgressBar />
-			<CursorGlow />
-			<CustomCursor />
-			<Header />
-			<Home />
-			<TechMarquee />
-			<About />
-			<Experience />
-			<Projects />
-			<Skills	/>
-			<ResearchAndDevelopment />
-			<Acamedic />
-			<Certifications />
-			<Footer />
-			<WhatsAppButton />
+			<ContactModalProvider>
+				<ScrollProgressBar />
+				<CursorGlow />
+				<CustomCursor />
+				<Header />
+				<Home />
+				<TechMarquee />
+				<About />
+				<Experience />
+				<Projects />
+				<Skills	/>
+				<ResearchAndDevelopment />
+				<Acamedic />
+				<Certifications />
+				<Footer />
+				<WhatsAppButton />
+				<ContactModalRoot />
+			</ContactModalProvider>
 		</LenisProvider>
 	)
 }
